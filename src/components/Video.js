652b27f29c68d93801fb77a137c958b4f1612fc0
suportdevs <suportdevs@gmail.com>
2022-07-03 07:35:38 +0600

    finish inifinite scoll use

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,20 +1,14 @@
-import { Link } from "react-router-dom";
 import classes from "../styles/Video.module.css";
 
 export default function Video({ title, noq, id }) {
   return (
-    <Link to="/quiz">
-      <div className={classes.video}>
-        <img
-          src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`}
-          alt=""
-        />
-        <p>{title}</p>
-        <div className={classes.qmeta}>
-          <p>{noq} Questions</p>
-          <p>Score : {10 * noq}</p>
-        </div>
+    <div className={classes.video}>
+      <img src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`} alt="" />
+      <p>{title}</p>
+      <div className={classes.qmeta}>
+        <p>{noq} Questions</p>
+        <p>Score : {10 * noq}</p>
       </div>
-    </Link>
+    </div>
   );
 }
--- src/components/Videos.js
+++ src/components/Videos.js
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { Link } from "react-router-dom";
 import useVideoList from "../hook/useVideoList";
 import Video from "./Video";
 
 export default function Videos() {
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState(8);
   const { videos, error, loading, hasMore } = useVideoList(page);
   return (
     <div>
@@ -14,18 +15,31 @@ export default function Videos() {
             dataLength={videos.length}
             hasMore={hasMore}
             next={() => setPage(page + 8)}
-            loader={<h4>Loading...</h4>}
+            // loader="Loading..."
           >
-            <Video
-              title={video.title}
-              key={video.youtubeID}
-              noq={video.noq}
-              id={video.youtubeID}
-            />
+            {video.noq > 0 ? (
+              <Link to="/quiz">
+                <Video
+                  title={video.title}
+                  key={video.youtubeID}
+                  noq={video.noq}
+                  id={video.youtubeID}
+                />
+              </Link>
+            ) : (
+              <Video
+                title={video.title}
+                key={video.youtubeID}
+                noq={video.noq}
+                id={video.youtubeID}
+              />
+            )}
           </InfiniteScroll>
         ))}
-      {!loading && videos.length === 0 && <div>Loading...</div>}
-      {!error && <div>No data found!</div>}
+      {!loading && videos.length === 0 && (
+        <div style={{ textAlign: "center" }}>Loading...</div>
+      )}
+      {!error && <div style={{ textAlign: "center" }}>No data found!</div>}
     </div>
   );
 }
